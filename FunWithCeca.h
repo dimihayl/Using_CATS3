08aa1b92ad4f1e77ee995995246ce64be652d83b
Dimitar Mihaylov <dimitar.mihaylov@mytum.de>
2025-04-22 15:09:24 +0200

    ceca for ml

--- FunWithCeca.h
+++ FunWithCeca.h
@@ -1,6 +1,9 @@
 #ifndef FUNWITHCECA_H
 #define FUNWITHCECA_H
 
+
+void CECA_sim_for_AI_pp_v1(int SEED, unsigned NUM_CPU, unsigned NUM_CONFIGS, unsigned K_YIELD_PER_CONFIG, TString OutputFolderName);
+
 int FUN_WITH_CECA(int narg, char** ARGS);
 
 #endif
